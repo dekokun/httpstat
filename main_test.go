1fa167f2e647b4c49d6d049c1b7aad69204e042c
dekokun <shintaro.kurachi@gmail.com>
2016-10-06 22:14:21 +0900

    テスト追加

--- main_test.go
+++ main_test.go
@@ -11,3 +11,17 @@ func Test引数がないとエラーになる(t *testing.T) {
 		t.Errorf("expected %d to eq %d", status, ExitCodeOK)
 	}
 }
+
+func Test存在しないステータスコードだとエラーになる(t *testing.T) {
+	args := strings.Split("httpstat 8888", " ")
+	if status := Run(args); status != ExitCodeError {
+		t.Errorf("expected %d to eq %d", status, ExitCodeError)
+	}
+}
+
+func Test404はOK(t *testing.T) {
+	args := strings.Split("httpstat 404", " ")
+	if status := Run(args); status != ExitCodeOK {
+		t.Errorf("expected %d to eq %d", status, ExitCodeOK)
+	}
+}
